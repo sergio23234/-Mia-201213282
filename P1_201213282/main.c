b3bc3e351506a9725e790f2f1fdc2e95a88a6f5b
sergio23234 <sergiofernando23234@hotmail.com>
2016-07-31 23:40:00 -0600

    actualizacion nocturna

--- P1_201213282/main.c
+++ P1_201213282/main.c
@@ -1,25 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+void fue_mkdisk(char cad[]){
+//printf("Resultado: %s\n",cad);
+char* part;
+part = strtok(cad," ");
+printf("veamos: %s",part);
+}
 void analizar_cadena(char cadena[]){
-printf("Resultado: \n%s",cadena);
-
+ printf("Resultado: %s\n",cadena);
+ char* pch;
+   pch = strtok(cadena," ");
+   if(strcmp(pch,"mkdisk")==0){
+   pch = strtok (NULL,"\n");
+   cadena = pch;
+   fue_mkdisk(cadena);
+   }
+   else if(strcmp(pch,"rmdisk")==0){
+   printf("rmdisk dos");}
+   else if(strcmp(pch,"fdisk")==0){
+   printf("fdsik tres");}
+   else if(strcmp(pch,"mount")==0){
+   printf("mount cuantro");}
+   else if(strcmp(pch,"umount")==0){
+   printf("umount cinco");}
+   else{
+        printf("error");
+   }
 }
 int main()
 {
     printf("introduzca comando \n");
-    char cadena[1000];
+    char cadena[1000]="";
     int i = 0;
     char c;
-    int a = 0;
-    while( (c = getchar())!='\n'){
-    cadena[i]=tolower(c);
+    int existe_cadena = 0;
+    int con_salto = 0;
+    int presiono_enter=1;
+    while(presiono_enter!=0){
+    if((c = getchar())!='\n'){
+    if(c==92){
+      cadena[i]=' ';
+      con_salto = 1;}
+    else{ cadena[i]=tolower(c);}
     i++;
-    a = 1;
+    existe_cadena = 1;}
+    else{
+    if(con_salto==0){presiono_enter=0;}
+    else{con_salto=0;}
     }
-    if(a==1){
-    analizar_cadena(&cadena);
+    }
+    if(existe_cadena==1){
+    analizar_cadena(cadena);
     }else{
    printf("Error");
     }
