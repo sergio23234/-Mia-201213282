aaf474c4c85499acc987f05fb7c48e27df4349b8
sergio23234 <sergiofernando23234@hotmail.com>
2016-08-01 16:28:36 -0600

    actualizando

--- P1_201213282/main.c
+++ P1_201213282/main.c
@@ -1,11 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void fue_mkdisk(char cad[]){
+void fue_mkdisk(char cad[], int size/*tamaño*/,int unit /*unidad*/,char path[]/*direccion*/,char nom[]/*nombre*/){
 //printf("Resultado: %s\n",cad);
-char* part;
-part = strtok(cad," ");
-printf("veamos: %s",part);
+char* prin/*cadena principal que utilizaremas*/; char* otro/*cadena extra que enviarmeos*/;
+prin = strtok(cad," ");
+otro = strtok(NULL,"\n");
+printf("\nveamos: %s",otro);
+char* sec;/*cadena secundaria derivada de la principal*/ char* ter; /*ultima cadena que utilizaremos*/
+int ver = 0;
+int comprobante = 0;
+char* aux = prin;
+while(*prin !=32&&comprobante==0){
+if(*prin==58){ver++;}
+if(ver==2){comprobante = 1;}
+prin++;}
+prin = aux;
+if(comprobante==1){ char *sec1;
+sec = strtok(prin,"::");
+sec1= strtok(NULL," ");
+ter = strtok(sec1,":");
+if(strcmp(sec,"-size")==0){
+int num=0;
+num = atoi(&ter);
+printf("\n%d y otro:%s",num,ter);
+}else if(strcmp(sec,"+unit")==0){}
+else if(strcmp(sec,"-path")==0){}
+else if(strcmp(sec,"-name")==0){}
+else{
+printf("Error");}
+}
+else{
+printf("Error");
+}
+
 }
 void analizar_cadena(char cadena[]){
  printf("Resultado: %s\n",cadena);
@@ -14,7 +42,7 @@ void analizar_cadena(char cadena[]){
    if(strcmp(pch,"mkdisk")==0){
    pch = strtok (NULL,"\n");
    cadena = pch;
-   fue_mkdisk(cadena);
+   fue_mkdisk(cadena,0,0/*Kilobytes*/,"","");
    }
    else if(strcmp(pch,"rmdisk")==0){
    printf("rmdisk dos");}
